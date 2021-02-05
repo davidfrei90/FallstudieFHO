29050686d0bbef5f9f537b5e4951c40f2c128b82
Fabian Rueesch <fabian.rueesch@outlookcom>
2021-02-05 10:30:45 +0100

    add Request Responses

---
+++ Application/Shared Libraries/HsrOrderApp.SharedLibraries.DTO/Requests_Responses/GetSupplierConditionResponse.cs
@@ -0,0 +1,22 @@
+﻿#region
+
+using System.Runtime.Serialization;
+using HsrOrderApp.SharedLibraries.DTO.Requests_Responses.Base;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+
+#endregion
+
+namespace HsrOrderApp.SharedLibraries.DTO.Requests_Responses
+{
+    public class GetSupplierConditionResponse : ResponseType
+    {
+        public GetSupplierConditionResponse()
+        {
+            this.SupplierCondition = new SupplierConditionDTO();
+        }
+
+        [DataMember]
+        [ObjectValidator]
+        public SupplierConditionDTO SupplierCondition { get; set; }
+    }
+}
---
+++ Application/Shared Libraries/HsrOrderApp.SharedLibraries.DTO/Requests_Responses/GetSupplierRequest.cs
@@ -0,0 +1,18 @@
+﻿#region
+
+using System.Runtime.Serialization;
+using HsrOrderApp.SharedLibraries.DTO.Requests_Responses.Base;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+
+#endregion
+
+namespace HsrOrderApp.SharedLibraries.DTO.Requests_Responses
+{
+    [DataContract]
+    public class GetSupplierRequest : RequestType
+    {
+        [DataMember]
+        [RangeValidator(0, RangeBoundaryType.Exclusive, int.MaxValue, RangeBoundaryType.Ignore)]
+        public int SupplierId { get; set; }
+    }
+}
