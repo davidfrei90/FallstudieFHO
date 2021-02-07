162cdd401e2646d653a2b6f22c8b4e90ac50e9b0
Fabian Rueesch <fabian.rueesch@outlookcom>
2021-02-07 16:13:54 +0100

    feat: add Connection Logic (Store, Get, Delete ) of Supplier and SupplierConditions

---
+++ Application/Shared Libraries/HsrOrderApp.SharedLibraries.DTO/Requests_Responses/StoreSupplierRequest.cs
@@ -0,0 +1,19 @@
+﻿#region
+
+using System.Runtime.Serialization;
+using HsrOrderApp.SharedLibraries.DTO.Requests_Responses.Base;
+using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+
+#endregion
+
+namespace HsrOrderApp.SharedLibraries.DTO.Requests_Responses
+{
+    [DataContract]
+    [KnownType(typeof (AddressDTO))]
+    public class StoreSupplierRequest : RequestType
+    {
+        [DataMember]
+        [ObjectValidator]
+        public SupplierDTO Supplier { get; set; }
+    }
+}
